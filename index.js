785e9577b7965979fea75d765861b99d8ace0299
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-02-01 22:29:55 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+function moveDodgerLeft(){
+    const leftNumber = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumber, 10);
+    if (left > 0){
+        dodger.style.left = `${left -1}px`;
+    }
+}
+function moveDodgerRight(){
+    const rightNumber = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumber, 10);
+    if (right < 360){
+        dodger.style.left = `${right + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function (e){
+    if (e.key ===  'ArrowLeft'){
+        moveDodgerLeft();
+    }else if (e.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+
+})
